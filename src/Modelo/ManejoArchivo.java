7e1a6ab5ef9a8ca4171f81f6d6223de423cb2b9b
AcostaF2001 <98819485+AcostaF2001@users.noreply.github.com>
2022-03-23 11:43:10 -0500

    cambios
    Goku le gana 5

--- src/Modelo/ManejoArchivo.java
+++ src/Modelo/ManejoArchivo.java
@@ -11,75 +11,78 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import javax.swing.JFileChooser;
-
+import javax.swing.filechooser.FileSystemView;
 
 /**
  *
  * @author andres_fel.acosta
  */
-//public class ManejoArchivo {
-//    public static boolean guardarArchivoXML(HashMap<String, LinkedList<Punto2D>> mp) {
-//        boolean t = false;
-//
-//        Element figurasG = new Element("FigurasGeometricas");
-//        Document document = new Document(figurasG);
-//
-//        Iterator<Map.Entry<String, LinkedList<Punto2D>>> entries = mp.entrySet().iterator();
-//
-//        while (entries.hasNext()) {
-//            Map.Entry<String, LinkedList<Punto2D>> entry = entries.next();
-//            String nombreF = entry.getKey();
-//            Element fg = new Element("FiguraGeometrica");
-//            fg.setAttribute("nombreF", nombreF);
-//            LinkedList<Punto2D> lp = entry.getValue();
-//
-//            for (int i = 0; i < lp.size(); i++) {
-//                Punto2D punto = entry.getValue().get(i);
-//                double x = punto.getX();
-//                double y = punto.getY();
-//                fg.addContent(new Element("Punto" + i).
-//                        setAttribute("X", String.valueOf(x)).
-//                        setAttribute("Y", String.valueOf(y)));
-//
-//                //System.out.println("Puntos "+punto.toString()+"\n");
-//            }
-//            document.getRootElement().addContent(fg);
-//            t = true;
-//            try{
-//                FileWriter writer = new FileWriter(ruta+"figurasG.xml");
-//                XMLOutputter outputter = new XMLOutputter();
-//                
-//            outputter.setFormat(Format.getCompactFormat().getPrettyFormat());
-//            outputter.output(document, writer);
-//            outputter.output(document, System.out);
-//                
-//            }catch(Exception e){
-//                System.out.println("Error "+e.toString());
-//            }
-//        }
-//
-//        return t;
-//
-//    }
-    
-//    public static boolean direccion(){
-//        boolean t=false;
-//        
-//        JFileChooser fc= new JFileChooser();
-//        fc.setCurrentDirectory(new File("."));
-//        fc.setDialogTitle("Seleccione carpeta donde desea guardar el archivo");
-//        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-//        fc.setAcceptAllFileFilterUsed(false);
-//        
-//        if (fc.showOpenDialog()=JFileChooser.APPROVE_OPTION) {
-//            
-//            File carpetaSeleccionada= fc.getCurrentDirectory();
-//            
-//        }
-//        
-//        
-//        
-//        return=t;
-//    }
-//}
+public class ManejoArchivo {
+
+    public static boolean guardarArchivoXML(HashMap<String, LinkedList<punto2D>> mp, String ruta) {
+        boolean t = false;
+
+        Element figurasG = new Element("FigurasGeometricas");
+        Document document = new Document(figurasG);
+
+        Iterator<Map.Entry<String, LinkedList<punto2D>>> entries = mp.entrySet().iterator();
+
+        while (entries.hasNext()) {
+            Map.Entry<String, LinkedList<punto2D>> entry = entries.next();
+            String nombreF = entry.getKey();
+            Element fg = new Element("FiguraGeometrica");
+            fg.setAttribute("nombreF", nombreF);
+            LinkedList<punto2D> lp = entry.getValue();
+
+            for (int i = 0; i < lp.size(); i++) {
+                punto2D punto = entry.getValue().get(i);
+                double x = punto.getX();
+                double y = punto.getY();
+                fg.addContent(new Element("Punto" + i).
+                        setAttribute("X", String.valueOf(x)).
+                        setAttribute("Y", String.valueOf(y)));
+
+                //System.out.println("Puntos "+punto.toString()+"\n");
+            }
+            document.getRootElement().addContent(fg);
+            t = true;
+            try {
+                FileWriter writer = new FileWriter(ruta + "figurasG.xml");
+                XMLOutputter outputter = new XMLOutputter();
+
+                outputter.setFormat(Format.getCompactFormat().getPrettyFormat());
+                outputter.output(document, writer);
+                outputter.output(document, System.out);
+
+            } catch (Exception e) {
+                System.out.println("Error " + e.toString());
+            }
+        }
+
+        return t;
+
+    }
+
+    public static String direccion() {
+
+        String ruta = null;
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+        int returnValue = jfc.showOpenDialog(null);
+        // int returnValue = jfc.showSaveDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            ruta = selectedFile.getAbsolutePath();
+        }
+
+        return ruta;
+
+    }
+
+}
